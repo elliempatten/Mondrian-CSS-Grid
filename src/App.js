6c01d775a720b29807443b63b8afd88e38719a51
Ellie Patten <ellie.patten@hotmail.co.uk>
2018-12-14 15:33:58 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
+const colours = [
+  // { colour: 'red', column: 2, row: 2, span: 4},
+  //{ colour: 'blue', column:  },
+
+  { colour: 'white', column: 1, row: 1, columnSpan: 2, rowSpan: 1 },
+  { colour: 'white', column: 3, row: 1, columnSpan: 3, rowSpan: 1 },
+  { colour: 'yellow', column: 6, row: 1, columnSpan: 2, rowSpan: 1 },
+  { colour: 'white', column: 8, row: 1, columnSpan: 1, rowSpan: 6 },
+  { colour: 'white', column: 1, row: 2, columnSpan: 1, rowSpan: 2 },
+  { colour: 'red', column: 2, row: 2, columnSpan: 4, rowSpan: 4 },
+  { colour: 'white', column: 6, row: 4, columnSpan: 2, rowSpan: 1 },
+  { colour: 'white', column: 6, row: 5, columnSpan: 2, rowSpan: 1 },
+  { colour: 'yellow', column: 6, row: 2, columnSpan: 2, rowSpan: 2 },
+  { colour: 'white', column: 1, row: 4, columnSpan: 1, rowSpan: 3 },
+  { colour: 'yellow', column: 1, row: 7, columnSpan: 1, rowSpan: 2 },
+  { colour: 'black', column: 2, row: 6, columnSpan: 2, rowSpan: 2 },
+  { colour: 'white', column: 4, row: 6, columnSpan: 2, rowSpan: 1 },
+  { colour: 'white', column: 4, row: 7, columnSpan: 2, rowSpan: 1 },
+  { colour: 'white', column: 6, row: 6, columnSpan: 2, rowSpan: 1 },
+  { colour: 'blue', column: 6, row: 7, columnSpan: 2, rowSpan: 2 },
+  { colour: 'yellow', column: 1, row: 7, columnSpan: 1, rowSpan: 3 },
+  { colour: 'black', column: 4, row: 8, columnSpan: 2, rowSpan: 1 },
+  { colour: 'red', column: 8, row: 7, columnSpan: 1, rowSpan: 3 },
+  { colour: 'white', column: 2, row: 8, columnSpan: 2, rowSpan: 2 },
+  { colour: 'white', column: 4, row: 9, columnSpan: 4, rowSpan: 1 }
+];
+// 8 columns
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="header">Responsive Mondrian with CSS grid</header>
+        <div className="main">
+          <div className="box">
+            {colours.map(colour => {
+              const classes = classNames(colour.colour, 'gridItem');
+              return (
+                <div
+                  className={classes}
+                  style={{
+                    gridColumn: `${colour.column} / span ${colour.columnSpan}`,
+                    gridRow: `${colour.row} / span ${colour.rowSpan}`
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <footer className="footer">
+          Made by{' '}
+          <a href="https://www.twitter.com/elliempatten">Ellie Patten</a>
+        </footer>
       </div>
     );
   }
